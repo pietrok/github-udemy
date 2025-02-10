914f42d7c04446b54821fb5ff39984eeac4f9214
Piet Heijdeman <piet.heijdeman@avalara.com>
2025-02-10 10:10:23 -0800

    chore: add workstation files for portability
    Files for HashiCorp Certified: Terraform Associate - Hands-On Labs

---
+++ workstation/tfbasics/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+##resource "random_string" "random" {
+##    length = 16 
+#}
